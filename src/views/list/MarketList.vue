d857d4490712666edcae677dfd3cc9e24f4742bd
ZhangS <376195987@qq.com>
2020-11-10 22:20:24 +0800

    update

--- src/views/list/MarketList.vue
+++ src/views/list/MarketList.vue
@@ -4,32 +4,32 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       treeData: [
         { title: 'Expand to load', key: '0' },
         { title: 'Expand to load', key: '1' },
-        { title: 'Tree Node', key: '2', isLeaf: true },
-      ],
-    };
+        { title: 'Tree Node', key: '2', isLeaf: true }
+      ]
+    }
   },
   methods: {
     onLoadData (treeNode) {
       return new Promise(resolve => {
         if (treeNode.dataRef.children) {
-          resolve();
-          return;
+          resolve()
+          return
         }
         setTimeout(() => {
           treeNode.dataRef.children = [
             { title: 'Child Node', key: `${treeNode.eventKey}-0` },
-            { title: 'Child Node', key: `${treeNode.eventKey}-1` },
-          ];
-          this.treeData = [...this.treeData];
-          resolve();
-        }, 1000);
-      });
-    },
-  },
-};
+            { title: 'Child Node', key: `${treeNode.eventKey}-1` }
+          ]
+          this.treeData = [...this.treeData]
+          resolve()
+        }, 1000)
+      })
+    }
+  }
+}
 </script>
