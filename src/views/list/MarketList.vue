2c82b17ff4079c3dd5538a6f07564937a8249e3b
ZhangS <376195987@qq.com>
2020-11-16 00:00:21 +0800

    update

--- src/views/list/MarketList.vue
+++ src/views/list/MarketList.vue
@@ -6,12 +6,16 @@
       :bordered="false"
       title="标准列表">
 
-      <a-tree :load-data="onLoadData" :tree-data="treeData" />
+      <a-tree
+        :load-data="onLoadData"
+        :tree-data="treeData"
+        :expanded-keys="expandedKeys"
+        :auto-expand-parent="true"
+        @select="onSelect" />
 
     </a-card>
   </page-header-wrapper>
 </template>
-
 <script>
 export default {
   data () {
@@ -20,22 +24,43 @@ export default {
         { title: 'Expand to load', key: '0' },
         { title: 'Expand to load', key: '1' },
         { title: 'Tree Node', key: '2', isLeaf: true }
-      ]
+      ],
+      expandedKeys: []
     }
   },
   methods: {
+    onSelect (selectedKeys, info) {
+      console.log('onSelect', selectedKeys, info)
+      selectedKeys.forEach(function (item) {
+        console.log(item)
+        this.expandedKeys.push(item)
+      }.bind(this))
+      console.log(this.expandedKeys)
+      this.selectedKeys = selectedKeys
+    },
+    onExpand (expandedKeys) {
+      console.log('onExpand', expandedKeys)
+      // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+      // or, you can remove all expanded children keys.
+      this.expandedKeys = expandedKeys
+      this.autoExpandParent = false
+    },
     onLoadData (treeNode) {
+      console.log(treeNode)
+      console.log(treeNode.dataRef.children)
       return new Promise(resolve => {
         if (treeNode.dataRef.children) {
           resolve()
           return
         }
-        treeNode.dataRef.children = [
-          { title: 'Child Node', key: `${treeNode.eventKey}-0` },
-          { title: 'Child Node', key: `${treeNode.eventKey}-1` }
-        ]
-        this.treeData = [...this.treeData]
-        resolve()
+        setTimeout(() => {
+          treeNode.dataRef.children = [
+            { title: 'Child Node', key: `${treeNode.eventKey}-0` },
+            { title: 'Child Node', key: `${treeNode.eventKey}-1` }
+          ]
+          this.treeData = [...this.treeData]
+          resolve()
+        }, 1000)
       })
     }
   }
