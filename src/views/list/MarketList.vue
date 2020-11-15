73aa05eb2858a9422f9e4b522c4e697c79c85a6c
ZhangS <376195987@qq.com>
2020-11-15 22:29:09 +0800

    Default Changelist

--- src/views/list/MarketList.vue
+++ src/views/list/MarketList.vue
@@ -6,59 +6,37 @@
       :bordered="false"
       title="标准列表">
 
-      <a-tree
-        checkable
-        :tree-data="treeData"
-        :default-expanded-keys="['0-0-0', '0-0-1']"
-        :default-selected-keys="['0-0-0', '0-0-1']"
-        :default-checked-keys="['0-0-0', '0-0-1']"
-        @select="onSelect"
-        @check="onCheck"
-      >
-        <span slot="title0010" style="color: #1890ff">sss</span>
-      </a-tree>
+      <a-tree :load-data="onLoadData" :tree-data="treeData" />
 
     </a-card>
   </page-header-wrapper>
 </template>
 
 <script>
-
-const treeData = [
-  {
-    title: 'parent 1',
-    key: '0-0',
-    children: [
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-        disabled: true,
-        children: [
-          { title: 'leaf', key: '0-0-0-0', disableCheckbox: true },
-          { title: 'leaf', key: '0-0-0-1' }
-        ]
-      },
-      {
-        title: 'parent 1-1',
-        key: '0-0-1',
-        children: [{ key: '0-0-1-0', slots: { title: 'title0010' } }]
-      }
-    ]
-  }
-]
-
 export default {
   data () {
     return {
-      treeData
+      treeData: [
+        { title: 'Expand to load', key: '0' },
+        { title: 'Expand to load', key: '1' },
+        { title: 'Tree Node', key: '2', isLeaf: true }
+      ]
     }
   },
   methods: {
-    onSelect (selectedKeys, info) {
-      console.log('selected', selectedKeys, info)
-    },
-    onCheck (checkedKeys, info) {
-      console.log('onCheck', checkedKeys, info)
+    onLoadData (treeNode) {
+      return new Promise(resolve => {
+        if (treeNode.dataRef.children) {
+          resolve()
+          return
+        }
+        treeNode.dataRef.children = [
+          { title: 'Child Node', key: `${treeNode.eventKey}-0` },
+          { title: 'Child Node', key: `${treeNode.eventKey}-1` }
+        ]
+        this.treeData = [...this.treeData]
+        resolve()
+      })
     }
   }
 }
